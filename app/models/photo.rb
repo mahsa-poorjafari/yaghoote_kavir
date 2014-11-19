708fec5f39793eb0be102f4aa45410d350051d71
mahsa <m_mw7462@yahoo.com>
2014-11-19 17:09:58 +0330

    fix bug in subgroups/show

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,7 @@
 # encoding: UTF-8
 class Photo < ActiveRecord::Base
+  extend FriendlyId  
+  friendly_id :title
   belongs_to :product
   has_attached_file :image, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
