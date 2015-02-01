2b8b954fd4bcb86dbbd5f8bdd730d93b8f8f8819
mahsa <m_mw7462@yahoo.com>
2015-02-01 12:02:52 +0330

    try to set responsive , fix design of gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,9 @@
 # encoding: UTF-8
 class Gallery < ActiveRecord::Base
-  has_attached_file :image, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>" }
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  has_attached_file :photo, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>" }
+  validates_attachment :photo, 
+    :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png"], :message => "فرمت عکس صحیح می باشد." },
+    :size => { :in => 0..300.kilobytes , :message => "حجم تصویر بیشتر از 300 کیلوبایت است."}
+  
+
 end
