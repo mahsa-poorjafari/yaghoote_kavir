942007b2f045e60c1b788c2aba8387e256959167
mahsa <m_mw7462@yahoo.com>
2015-02-06 13:42:16 +0330

    add download link for originial size of products image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ActiveRecord::Base
   extend FriendlyId  
   friendly_id :title
   belongs_to :product
-  has_attached_file :image, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>" }
+  has_attached_file :image, :styles => { :medium => "450x450>", :small => "300x350>" }
   validates_attachment :image, 
     :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png"], :message => "فرمت عکس صحیح می باشد." },
     :size => { :in => 0..300.kilobytes , :message => "حجم تصویر بیشتر از 300 کیلوبایت است."}
