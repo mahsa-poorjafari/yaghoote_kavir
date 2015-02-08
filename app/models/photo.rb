892267ade7860d4d3be2182d0af2ea3afdf0c83f
mahsa <m_mw7462@yahoo.com>
2015-02-08 11:32:36 +0330

    remove original limit from photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,7 +8,7 @@ class Photo < ActiveRecord::Base
     :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png"], :message => "فرمت عکس صحیح می باشد." },
     :size => { :in => 0..300.kilobytes , :message => "حجم تصویر بیشتر از 300 کیلوبایت است."}
   
-  has_attached_file :second_img, :styles => { :original => "700x650>" , :medium => "450x450>", :small => "300x350>" }
+  has_attached_file :second_img, :styles => {  :medium => "450x450>", :small => "300x350>" }
   validates_attachment :second_img, 
     :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png"], :message => "فرمت عکس صحیح می باشد." },
     :size => { :in => 0..300.kilobytes , :message => "حجم تصویر بیشتر از 300 کیلوبایت است."}
